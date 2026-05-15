ccd39258b2868b81e63dbe0548dc0dca5e9ae1e0
Gabriel Benedito <gabrielh.beneditoo@gmail.com>
2026-05-15 16:45:55 -0300

    Rota fazer cadastro

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -31,3 +31,35 @@ def tela_login(request: Request):
         "auth/login.html",
         {"request": request}
     )
+
+
+# ROTA PARA CRIAR O USUÁRIO
+@router.post("/cadastro")
+def fazer_cadastro(
+    request: Request,
+    nome: str = Form(...),
+    email: str = Form(...),
+    senha: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    # Verificar se o email esta cadastrado
+    usuario_existente = db.query(Usuario).filter_by(email=email).first()
+
+    # Mensagem de erro se o email já estiver cadastrado
+    if usuario_existente:
+        return templates.TemplateResponse(
+            request,
+            "auth/cadastro.html",
+            {"request": request, "erro": "Email já cadastrado"}
+        )
+    
+    # Criar usuario - criar objeeto
+    novo_usuario = Usuario(
+        nome=nome,
+        email=email,
+        senha_hash=hash_senha(senha)
+    )
+    db.add(novo_usuario)
+    db.commit()
+
+    return RedirectResponse(url="/auth/login", status_code=302)   
